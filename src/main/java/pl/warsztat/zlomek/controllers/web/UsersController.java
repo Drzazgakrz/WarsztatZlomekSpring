12eb59aff468add05d83760be73aac82a8602c6c
Krzysztof Drzazga <drzazga_krzysztof@o2.pl>
2019-05-22 10:44:33 +0200

    Couple changes

--- src/main/java/pl/warsztat/zlomek/controllers/web/UsersController.java
+++ src/main/java/pl/warsztat/zlomek/controllers/web/UsersController.java
@@ -3,11 +3,15 @@ package pl.warsztat.zlomek.controllers.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.warsztat.zlomek.data.ClientRepository;
 import pl.warsztat.zlomek.model.db.Client;
 import pl.warsztat.zlomek.model.db.ClientStatus;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,4 +41,14 @@ public class UsersController {
         return "users";
     }
 
+    @RequestMapping(path = "clientDetails", method = RequestMethod.GET)
+    public String getClientDetails(Model model, @RequestParam long clientId){
+        model.addAttribute("account", clientRepository.getClientById(clientId));
+        return "clientDetails";
+    }
+
+    @RequestMapping(path = "update", method = RequestMethod.POST)
+    public String updateClientData(@Valid Client client){
+        return "users";
+    }
 }
